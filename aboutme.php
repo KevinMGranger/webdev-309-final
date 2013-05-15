e766fa08ee65e2a06d3fdbcace084e4eca1e3098
Kevin Granger <kmg2728@rit.edu>
2013-05-15 10:58:55 -0400

    Created aboutme page. 90% complete with document.

---
+++ aboutme.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Kevin Granger: About Me</title>
+		<link rel="stylesheet/less" type="text/css" href="./css/index.less">
+		<?php include './lib/inc/masterstyle.html'; ?>
+	</head>
+	<body>
+		<div class='container'>
+			<?php include './lib/inc/nav.html'; ?>
+
+			<section>
+			<h2>I'm Kevin, pleased to meet you.</h2>
+
+			<p>I'm an aspiring developer and designer of all things interactive.</p>
+			</section>
+
+			<section>
+			<h2>My Dream</h2>
+
+
+			<p>The late Roger Ebert, a brilliant critic, once said that
+
+			<q cite='http://www.rogerebert.com/rogers-journal/video-games-can-never-be-art'>
+				<a href='http://www.rogerebert.com/rogers-journal/video-games-can-never-be-art'>
+					"video games can never be art"
+				</a>
+			</q>.
+
+			He later <a href='http://www.rogerebert.com/rogers-journal/video-games-can-never-be-art'>changed his position</a>, but his words still opened up a great debate on the topic.
+
+			</p>
+
+
+			<p>While I will mostly agree in his assertion that
+
+			<q cite='http://www.rogerebert.com/answer-man/why-did-the-chicken-cross-the-genders'>
+				<a href='http://www.rogerebert.com/answer-man/why-did-the-chicken-cross-the-genders'>
+					"...no one in or out of the field has ever been able to cite a game worthy of comparison with the great dramatists, poets, filmmakers, novelists and composers"
+				</a>
+			</q>,
+
+			I think that games as art are 
+			<a href='http://www.youtube.com/watch?v=K9y6MYDSAww'>still in their infancy</a>.
+			</p>
+
+			<p>
+			<strong>It is my dream to change that.</strong>
+			</p>
+			</section>
+
+			<section>
+			        <h2>Schooling</h2>
+			        <p></p>
+			        <p></p>
+			</section>
+
+			<section>
+			<h2>Leadership</h2>
+			<p></p>
+
+			<p>I'm involved with <del>too many</del> <ins>a multitude of</ins> different organizations on campus:</p>
+
+			<ul>
+				<li>Undergraduate Admissions:
+				<ul>
+					<li>Student Ambassador (Tour Guide)</li>
+					<li>Social Media <a href="http://www.youtube.com/5ritstudents">(#RITStudents)</a></li>
+				</ul>
+				</li>
+
+				<li>Sigma Chi Fraternity - Lambda Kappa Chapter: Pro Consul (Vice President)</li>
+
+				<li>RIT Linux Users Group: President</li>
+
+				<li>New Student Orientation: Lead Orientation Assistant</li>
+			</ul>
+			</section>
+
+		</div>
+	</body>
+</html>
+
+<!-- vim:ft=html-->
