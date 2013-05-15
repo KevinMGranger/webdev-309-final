7036e4f2615983efefc907bb2e9eb44555f10b81
Kevin Granger <kmg2728@rit.edu>
2013-05-15 09:36:50 -0400

    Created beginnings of main index & it's less/css.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Kevin Granger</title>
+		<link rel="stylesheet/less" type="text/css" href="./css/index.less">
+		<?php include './lib/inc/masterstyle.html'; ?>
+	</head>
+	<body>
+		<div class='container'>
+			<nav>
+			<?php include './lib/inc/nav.html'; ?>
+			</nav>
+		</div>
+	</body>
+</html>
+
+<!-- vim:set ft=html-->
