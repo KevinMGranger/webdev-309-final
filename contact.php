7b2856320ea7ff8a45c37b4f91fe1c7f8c13084b
Kevin Granger <kmg2728@rit.edu>
2013-05-16 00:06:04 -0400

    Created beginnings of contact page and non-scrapeable population script.

---
+++ contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Kevin Granger: Contact</title>
+		<?php include './lib/inc/masterstyle.html'; ?>
+	</head>
+	<body>
+		<div class='container'>
+			<?php include './lib/inc/nav.html'; ?>
+			<div class='phone'>
+			        <a href=''>
+			                <img src='./img/icons/' alt='' />
+			        </a>
+			        <a href=''>
+			                <p>These</p>
+			        </a>
+			</div>
+			<div class='mail'>
+			        <a href=''>
+			                <img src='./img/icons/' alt='' />
+			        </a>
+			        <a href=''>
+			                <p>are</p>
+			        </a>
+			</div>
+			<div class='face'>
+			        <a href=''>
+			                <img src='./img/icons/' alt='' />
+			        </a>
+			        <a href=''>
+			                <p>filled w/</p>
+			        </a>
+			</div>
+			<div class='twit'>
+			        <a href=''>
+			                <img src='./img/icons/' alt='' />
+			        </a>
+			        <a href=''>
+			                <p>JavaScript</p>
+			        </a>
+			</div>
+		</div>
+
+		<script src="./lib/js/jquery.js"></script>
+		<script src="./lib/js/populatecontact.js"></script>
+	</body>
+</html>
+
+<!-- vim: set ft=html:-->
