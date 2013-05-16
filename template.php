a1d51971b517664d0be86a1311f4f8933242b5a1
Kevin Granger <kmg2728@rit.edu>
2013-05-16 00:03:27 -0400

    Created template

---
+++ template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Kevin Granger</title>
+		<?php include '/webdev-309/final/lib/inc/masterstyle.html'; ?>
+	</head>
+	<body>
+		<div class='container'>
+			<?php include '/webdev-309/final/lib/inc/nav.html'; ?>
+		</div>
+	</body>
+</html>
+
+<!-- vim: set ft=html:-->
