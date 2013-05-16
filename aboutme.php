53c35a16dced3680e84e9ec000f34ea7fa65dba6
Kevin Granger <kmg2728@rit.edu>
2013-05-16 00:04:48 -0400

    Updated About Me: added interests, involvement, touched up some markdown in quotes sections.

--- aboutme.php
+++ aboutme.php
@@ -10,6 +10,8 @@
 		<div class='container'>
 			<?php include './lib/inc/nav.html'; ?>
 
+			<img src='./img/kevin' alt='' title='Kevin Granger' />
+
 			<section>
 			<h2>I'm Kevin, pleased to meet you.</h2>
 
@@ -24,12 +26,9 @@
 
 			<q cite='http://www.rogerebert.com/rogers-journal/video-games-can-never-be-art'>
 				<a href='http://www.rogerebert.com/rogers-journal/video-games-can-never-be-art'>
-					"video games can never be art"
-				</a>
-			</q>.
+					"video games can never be art"</a></q>.
 
 			He later <a href='http://www.rogerebert.com/rogers-journal/video-games-can-never-be-art'>changed his position</a>, but his words still opened up a great debate on the topic.
-
 			</p>
 
 
@@ -37,11 +36,9 @@
 
 			<q cite='http://www.rogerebert.com/answer-man/why-did-the-chicken-cross-the-genders'>
 				<a href='http://www.rogerebert.com/answer-man/why-did-the-chicken-cross-the-genders'>
-					"...no one in or out of the field has ever been able to cite a game worthy of comparison with the great dramatists, poets, filmmakers, novelists and composers"
-				</a>
-			</q>,
+					"...no one in or out of the field has ever been able to cite a game worthy of comparison with the great dramatists, poets, filmmakers, novelists and composers"</a></q>,
 
-			I think that games as art are 
+			I think that games as art are
 			<a href='http://www.youtube.com/watch?v=K9y6MYDSAww'>still in their infancy</a>.
 			</p>
 
@@ -51,33 +48,56 @@
 			</section>
 
 			<section>
-			        <h2>Schooling</h2>
-			        <p></p>
-			        <p></p>
+			<h2>Schooling</h2>
+			<p>I am a third-year student at Rochester Institute of Technology (RIT). I am pursuing a Bachelors of Science in Game Design and Development.</p>
+			<p>I am also a former Physics major. As much as I still love physics, I realized that my passion lied elsewhere.</p>
+			<p>At least I still have a love for things on the technical side.</p>
 			</section>
 
 			<section>
 			<h2>Leadership</h2>
 			<p></p>
 
-			<p>I'm involved with <del>too many</del> <ins>a multitude of</ins> different organizations on campus:</p>
+			<p>I am currently, and have been, involved with <del>too many</del> <ins>a multitude of</ins> different organizations on campus:</p>
 
 			<ul>
-				<li>Undergraduate Admissions:
+				<li>Undergraduate Admissions: (Current)
 				<ul>
 					<li>Student Ambassador (Tour Guide)</li>
-					<li>Social Media <a href="http://www.youtube.com/5ritstudents">(#RITStudents)</a></li>
+					<li>Social Media Ambassador <a href="http://www.youtube.com/5ritstudents">(#RITStudents)</a></li>
 				</ul>
 				</li>
 
-				<li>Sigma Chi Fraternity - Lambda Kappa Chapter: Pro Consul (Vice President)</li>
+				<li>Sigma Chi Fraternity - Lambda Kappa Chapter: 
+				<ul>
+					<li>Pro Consul (Vice President) (Current)</li>
+					<li>Risk Manager (Past)</li>
+					<li>Chapter Editor (worked on newsletters) (Past)</li>
+					<li>Kustos (kept track of ritual regalia) (Past)</li>
+				</ul>
+				</li>
 
-				<li>RIT Linux Users Group: President</li>
+				<li>RIT Linux Users Group: President (Past)</li>
 
-				<li>New Student Orientation: Lead Orientation Assistant</li>
+				<li>New Student Orientation: 
+				<ul>
+					<li>Lead Orientation Assistant (Current)</li>
+					<li>Freshman Orientation Assistant (Past)</li>
+				</ul>
+				</li>
 			</ul>
 			</section>
 
+			<section>
+			<h2>Interests</h2>
+			<p>When I manage to find some free time (never), I also have some assorted interests:</p>
+			<ul>
+				<li>FOSS (Free and Open Source Software). Free as in Speech, not as in Beer. I love the concept of collaborative problem solving through technology, and the sharing of practical work.</li>
+				<li>GNU/Linux. I love using being able to customize almost every aspect of my operating system, and the idea that it's free (in both senses of the word).</li>
+				<li>Gaming. A Game Design major who likes to play games, imagine that?</li>
+				<li>The Great Outdoors. Camping, hiking, swimming, sailing. Once a Boy Scout, always a Boy Scout. You can't spend all of your time in front of a computer, can you?</li>
+			</ul>
+			</section>
 		</div>
 	</body>
 </html>
