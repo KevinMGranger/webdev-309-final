5851e3b0fc15c6966d20f57f8f30e952f6f0d767
Kevin Granger <kmg2728@rit.edu>
2013-05-27 19:40:48 -0400

    Removed unnecessary <p>'s from contact page, cited glyphicons and made wrapper.

--- contact.php
+++ contact.php
@@ -5,42 +5,38 @@
 		<title>Kevin Granger: Contact</title>
 		<?php include './lib/inc/fonts.html' ?>
 		<link rel="stylesheet/less" type="text/css" href="./css/contact.less">
-		<script src="/webdev-309/final/lib/js/less.min.js" type="text/javascript"></script>
+		<script src="/~kmg2728/webdev-309/final/lib/js/less.min.js" type="text/javascript"></script>
 	</head>
 	<body>
 			<?php include './lib/inc/nav.html'; ?>
+			<article>
 			<div class='contactblock phone'>
 				<a href=''>
 					<img src='./img/icons/phone' alt='' />
 				</a>
-				<a href=''>
-					<p>These</p>
-				</a>
+				<a href=''>These</a>
 			</div>
 			<div class='contactblock mail'>
 				<a href=''>
 					<img src='./img/icons/mail' alt='' />
 				</a>
-				<a href=''>
-					<p>are</p>
-				</a>
+				<a href=''>are</a>
 			</div>
 			<div class='contactblock face'>
 				<a href=''>
 					<img src='./img/icons/facepod' alt='' />
 				</a>
-				<a href=''>
-					<p>filled w/</p>
-				</a>
+				<a href=''>filled w/</a>
 			</div>
 			<div class='contactblock twit'>
 				<a href=''>
 					<img src='./img/icons/birdie' alt='' />
 				</a>
-				<a href=''>
-					<p>JavaScript</p>
-				</a>
+				<a href=''>JavaScript</a>
 			</div>
+
+			<p>Icons from <a href='http://glyphicons.com/'>Glyphicons</a>.</p>
+			</article>
 
 		<?php include './lib/inc/navhandler.html' ?>
 		<script src="./lib/js/populatecontact.js"></script>
