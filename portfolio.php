c95be03cfb9ba9cf4dfbca1bbd40e4aeec82f336
Kevin Granger <kmg2728@rit.edu>
2013-05-28 12:46:43 -0400

    Made portfolio page

---
+++ portfolio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Kevin Granger: Portfolio</title>
+		<?php include './lib/inc/fonts.html'; ?>
+		<link rel="stylesheet/less" type="text/css" href="./css/portfolio.less">
+		<script src="/~kmg2728/webdev-309/final/lib/js/less.min.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<?php include './lib/inc/nav.html'; ?>
+
+		<article>
+
+		<section>
+		        <h2>Personal Site</h2>
+		        <img src='/~kmg2728/webdev-309/final/img/thissite.png' alt='A screenshot of this site.' />
+		        <p>I made a personal site for my Intro to Web Dev class. pretty nifty, eh?</p>
+		</section>
+
+		<section>
+		        <h2>Placeholder!</h2>
+		        <img src='/~kmg2728/webdev-309/final/img/ipsum.png' alt='A screenshot of Lorem Ipsum.' />
+		        <p>I'm a first year game design student, so there's not much in my portfolio. So here's a picture of some lorem ipsum.</p>
+		</section>
+		
+
+
+		</article>
+
+		<?php include './lib/inc/navhandler.html' ?>
+	</body>
+</html>
+
+<!-- vim: set ft=html:-->
